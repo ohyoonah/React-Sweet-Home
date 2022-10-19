1776683882675867f8a78b1ef3756d9f3986b3ff
ohyoonah <dhdbsdk3@naver.com>
2022-10-19 19:24:54 +0900

    이미지 슬라이더 배포용 수정

--- src/components/slide/ImageSlider_firebase.js
+++ src/components/slide/ImageSlider_firebase.js
@@ -3,21 +3,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import { db } from "../../Firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 const SliderBlock = styled(Slider)`
   text-align: center;
   overflow-x: hidden;
-  .slick-dots {
-    position: absolute;
-    bottom: 2rem;
-    @media only screen and (max-width: 1024px) {
-      bottom: 1rem;
-    }
-  }
-  .slick-dots li button:before {
-    color: var(--light-gray);
-  }
 `;
 
 const ImageBlock = styled.img`
@@ -28,25 +19,22 @@ const ImageBlock = styled.img`
     height: 270px;
   }
   @media only screen and (max-width: 768px) {
-    height: 320px;
+    height: 440px;
   }
 `;
 
 const ImageSlider = () => {
   const [width, setWidth] = useState(window.innerWidth < 768 ? true : false);
-  const [images, setImages] = useState(null);
+  const [image, setImage] = useState([]);
+  const collectionRef = collection(db, "image_slider");
 
   useEffect(() => {
     const getData = async () => {
-      const {
-        data: { banners },
-      } = await axios.get("/api/getItemApi");
-      setImages(banners);
+      const data = await getDocs(collectionRef);
+      setImage(data.docs.map((doc) => ({ ...doc.data() })));
     };
     getData();
-  }, []);
-
-  console.log(images);
+  }, [collectionRef]);
 
   const screenChange = (e) => {
     const matches = e.matches;
@@ -60,7 +48,7 @@ const ImageSlider = () => {
   }, []);
 
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -73,17 +61,15 @@ const ImageSlider = () => {
     <div>
       {width ? (
         <SliderBlock {...settings}>
-          {images &&
-            images.map(({ id, badge_title, mobile_image }) => (
-              <ImageBlock key={id} src={mobile_image.url} alt={badge_title} />
-            ))}
+          {image.map(({ id, name, image2 }) => (
+            <ImageBlock key={id} src={image2} alt={name} />
+          ))}
         </SliderBlock>
       ) : (
         <SliderBlock {...settings}>
-          {images &&
-            images.map(({ id, badge_title, pc_image }) => (
-              <ImageBlock key={id} src={pc_image.url} alt={badge_title} />
-            ))}
+          {image.map(({ id, name, image }) => (
+            <ImageBlock key={id} src={image} alt={name} />
+          ))}
         </SliderBlock>
       )}
     </div>
